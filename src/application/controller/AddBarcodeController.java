659a8220e732e736a00aad7e0441d8230bbd6d83
Andrew Waite <drewrockz01@gmail.com>
2017-05-17 21:15:17 +1000

    Added AddBarcodeController

---
+++ src/application/controller/AddBarcodeController.java
@@ -0,0 +1,16 @@
+package application.controller;
+
+public class AddBarcodeController
+{
+	//Called before all FXML members have been injected
+	public AddBarcodeController()
+	{
+		
+	}
+	
+	//Called after all FXML members have been injected
+	public void initialize()
+	{
+		
+	}
+}
