c2663989287d7e879b631886dcbf7ae88e0f278b
Andrew Waite <drewrockz01@gmail.com>
2017-05-17 21:08:11 +1000

    Added ItemDetailController

---
+++ src/application/controller/ItemDetailController.java
@@ -0,0 +1,15 @@
+package application.controller;
+
+public class ItemDetailController 
+{
+	//Called before all FXML members have been injected
+	public ItemDetailController()
+	{
+		
+	}
+	
+	//Called after all FXML members have been injected
+	public void initialize()
+	{
+	}
+}
