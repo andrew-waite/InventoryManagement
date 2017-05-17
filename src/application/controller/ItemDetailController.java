2048857a9e8db36aa8082f32ff64ed0e76f38496
Andrew Waite <drewrockz01@gmail.com>
2017-05-17 21:19:50 +1000

    Initialized AddBarcode Layout and Controller

--- src/application/controller/ItemDetailController.java
+++ src/application/controller/ItemDetailController.java
@@ -1,5 +1,14 @@
 package application.controller;
 
+import java.io.IOException;
+
+import application.Main;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
 public class ItemDetailController 
 {
 	//Called before all FXML members have been injected
@@ -12,5 +21,26 @@ public class ItemDetailController
 	public void initialize()
 	{
 		
+	}
+	
+	@FXML
+	public void launchAddBarcode()
+	{
+		 try 
+	     {
+			 Stage stage = new Stage();
+	        	
+			 // Load root layout from fxml file.
+	         FXMLLoader loader = new FXMLLoader(Main.class.getResource("view/AddBarcodeLayout.fxml"));
+	         AnchorPane itemDetailLayout = (AnchorPane) loader.load();
+
+	         Scene scene = new Scene(itemDetailLayout, 522, 200);
+	         stage.setScene(scene);
+	         stage.show();
+	     } 
+	     catch (IOException e) 
+	     {
+	    	 e.printStackTrace();
+		 }
 	}
 }
