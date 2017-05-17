7b78bfa938b4e1e7df40f5e5ab27de4a9ca70782
Andrew Waite <drewrockz01@gmail.com>
2017-05-17 19:08:36 +1000

    Added basic functionallity to update tableview programatically

--- src/application/controller/RootLayoutController.java
+++ src/application/controller/RootLayoutController.java
@@ -1,13 +1,26 @@
 package application.controller;
 
+import application.TableRowData;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.StackPane;
 
 public class RootLayoutController extends StackPane
 {
 	@FXML
 	private Button asdf;
+	
+	@FXML 
+	private TableView<TableRowData> tableView;
+	
+	@FXML
+	private TableColumn<TableRowData, String> tableColumn;
 	
 	public RootLayoutController()
 	{
@@ -20,8 +33,17 @@ public class RootLayoutController extends StackPane
 	}
 
 	@FXML
-	private void buttonClicked()
+	private void clickedButton(ActionEvent event)
 	{
+		System.out.println("Something");
+		
+		
+		ObservableList<TableRowData> data = FXCollections.observableArrayList();
+		data.add(new TableRowData("HI"));
+		
+		tableColumn.setCellValueFactory(new PropertyValueFactory<TableRowData, String>("description"));
+		
+		tableView.setItems(data);
 		
 	}
 }
